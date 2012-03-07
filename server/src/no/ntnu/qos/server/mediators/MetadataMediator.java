b26b8b66ddb9b73774658ceb2bfe9b576502365e
Ola Martin Tiseth Støvneng <ola.martin.st@gmail.com>
2012-03-07 12:04:30 +0100

    created MetadataMediator

---
+++ server/src/no/ntnu/qos/server/mediators/MetadataMediator.java
@@ -0,0 +1,14 @@
+package no.ntnu.qos.server.mediators;
+
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.mediators.AbstractMediator;
+
+public class MetadataMediator extends AbstractMediator {
+
+	@Override
+	public boolean mediate(MessageContext arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
