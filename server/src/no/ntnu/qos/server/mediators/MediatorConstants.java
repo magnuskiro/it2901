d85a295cd730c43c7fce1e4df8ba84a592d61d64
Ola Martin Tiseth Støvneng <ola.martin.st@gmail.com>
2012-03-07 12:34:34 +0100

    added MediatorConstants

---
+++ server/src/no/ntnu/qos/server/mediators/MediatorConstants.java
@@ -0,0 +1,6 @@
+package no.ntnu.qos.server.mediators;
+
+public final class MediatorConstants {
+	public static final String CLIENT_ROLE = "clientRole";
+	public static final String SERVICE = "service";
+}
--- server/src/no/ntnu/qos/server/mediators/MetadataMediator.java
+++ server/src/no/ntnu/qos/server/mediators/MetadataMediator.java
@@ -1,14 +1,46 @@
 package no.ntnu.qos.server.mediators;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.SynapseLog;
 import org.apache.synapse.mediators.AbstractMediator;
+import org.apache.synapse.mediators.MediatorProperty;
 
 public class MetadataMediator extends AbstractMediator {
 
+	public final static String FILENAME = "filename";
+	private final List<MediatorProperty> properties = new ArrayList<MediatorProperty>();
 	@Override
-	public boolean mediate(MessageContext arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean mediate(MessageContext synCtx) {
+		
+		SynapseLog synLog = getLog(synCtx);
+
+        if (synLog.isTraceOrDebugEnabled()) {
+            synLog.traceOrDebug("Start : Metadata mediator");
+
+            if (synLog.isTraceTraceEnabled()) {
+                synLog.traceTrace("Message : " + synCtx.getEnvelope());
+            }
+        }
+        
+        final String clientRole = (String)synCtx.getProperty(MediatorConstants.CLIENT_ROLE);
+        final String service = (String)synCtx.getProperty(MediatorConstants.SERVICE);
+        
+		return true;
+	}
+	
+	public void addProperty(MediatorProperty mp){
+		properties.add(mp);
+	}
+	
+	public void addAllProperties(List<MediatorProperty> lmp){
+		properties.addAll(lmp);
+	}
+	
+	public List<MediatorProperty> getProperties(){
+		return properties;
 	}
 
 }
