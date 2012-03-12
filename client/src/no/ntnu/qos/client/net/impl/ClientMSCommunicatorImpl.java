fed9ee8f953eeca262b1f6d9ff572e6b733c1fca
Haavard Toerresen <skabbkladden@gmail.com>
2012-03-12 13:56:54 +0100

    renamed MSCommunicator to ClientMSCommunicator, a bit more implementation

---
+++ client/src/no/ntnu/qos/client/net/impl/ClientMSCommunicatorImpl.java
@@ -0,0 +1,50 @@
+package no.ntnu.qos.client.net.impl;
+
+
+import no.ntnu.qos.client.DataObject;
+import no.ntnu.qos.client.net.ClientMSCommunicator;
+
+
+/**
+ * Implementation of the no.ntnu.qos.client.net.MSCommunicator interface
+ * @author Håvard
+ *
+ */
+public class ClientMSCommunicatorImpl implements ClientMSCommunicator{
+
+	private String ms;
+	
+	/**
+	 * main constructor
+	 */
+	public ClientMSCommunicatorImpl(String newMS){
+		ms	= newMS;
+	}
+	
+	@Override
+	public Runnable getRouteInfo(DataObject dataObj) {		
+		return new RouteFetcher(dataObj);
+	}
+
+	/**
+	 * private runnable that will be returned by the getRouteInfo method
+	 * @author Håvard
+	 *
+	 */
+	private class RouteFetcher implements Runnable{
+
+		private DataObject dataObj;
+		
+		public RouteFetcher(DataObject data){
+			dataObj = data;
+		}
+		
+		@Override
+		public void run() {
+			// TODO fetch the routing info and insert it into the DataObject
+			//MSCommunicator meh = new MSCommunicatorImlp(ms);
+			//dataObj.setRouteInfo(meh.getblah());
+		}
+		
+	}
+}
