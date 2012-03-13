b41086e77602693d1ad19ceb69c1d436b33a9d01
Haavard Toerresen <skabbkladden@gmail.com>
2012-03-13 13:59:09 +0100

    javadoc

--- client/src/no/ntnu/qos/client/SanityChecker.java
+++ client/src/no/ntnu/qos/client/SanityChecker.java
@@ -8,7 +8,18 @@ package no.ntnu.qos.client;
  */
 public interface SanityChecker {
 
+	/**
+	 * checks the given DataObject for correctness and calls setSane with the result
+	 * @param data	- the DataObject to check
+	 */
 	public void isSane(DataObject data);
 	
+	/**
+	 * checks the given credential triplet for obvious faults
+	 * @param userName	- client username
+	 * @param password	- client password
+	 * @param role		- client role
+	 * @return	true if sane, else: false
+	 */
 	public boolean isSane(String userName, String password, String role);
 }
