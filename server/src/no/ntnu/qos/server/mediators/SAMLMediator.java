750e8b175a66a6866f2d494a961a3eec1d4427b9
Jørgen Nordmoen <nordmoen90@gmail.com>
2012-03-13 10:24:30 +0100

    Added some comments and fixed up the Test a bit

--- server/src/no/ntnu/qos/server/mediators/SAMLMediator.java
+++ server/src/no/ntnu/qos/server/mediators/SAMLMediator.java
@@ -3,6 +3,16 @@ package no.ntnu.qos.server.mediators;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.mediators.AbstractMediator;
 
+/**
+ * This mediator is meant to parse the message as it is coming in through
+ * the ESB and extract the SAML message to determine the role of the client.
+ * This mediator will also extract the service the client is trying to invoke
+ * and add both to the properties of the {@link MessageContext} so we can
+ * retrieve in the outsequence. 
+ * 
+ * @author Jørgen & Ola Martin
+ *
+ */
 public class SAMLMediator extends AbstractMediator {
 
 	@Override
--- server/src/no/ntnu/qos/server/mediators/test/SAMLMediatorTest.java
+++ server/src/no/ntnu/qos/server/mediators/test/SAMLMediatorTest.java
@@ -15,22 +15,22 @@ import org.junit.Test;
 public class SAMLMediatorTest {
 
 	private static MessageContext synCtx;
-	private static final String ADDRESS = "127.0.0.1";
-	private static final String SERVICE = "serviceName1";
+	private static final String CLIENT_ROLE = "client_role1";
+	private static final String SERVICE = "127.0.0.1";
 
 	@BeforeClass
 	public static void setupMessageContext(){
 		synCtx = new Axis2MessageContext(new org.apache.axis2.context.MessageContext(), 
 				new SynapseConfiguration(), null);
 		//Configure the message context
-		synCtx.setTo(new EndpointReference(ADDRESS));
+		synCtx.setTo(new EndpointReference(SERVICE));
 	}
 
 	@Test
 	public void testMediate() {
 		SAMLMediator sm = new SAMLMediator();
 		assertTrue("Mediate synapse context", sm.mediate(synCtx));
-		assertEquals("Client role", ADDRESS, synCtx.getProperty(MediatorConstants.QOS_CLIENT_ROLE));
+		assertEquals("Client role", CLIENT_ROLE, synCtx.getProperty(MediatorConstants.QOS_CLIENT_ROLE));
 		assertEquals("Service", SERVICE, synCtx.getProperty(MediatorConstants.QOS_SERVICE));
 	}
 
