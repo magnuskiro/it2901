25c5fba77767c89e6514dec5239e83a22bb8140d
Jørgen Nordmoen <nordmoen90@gmail.com>
2012-03-13 11:48:22 +0100

    Updated test, it should now work as long as the new xml file is found

--- server/src/no/ntnu/qos/server/mediators/test/SAMLMediatorTest.java
+++ server/src/no/ntnu/qos/server/mediators/test/SAMLMediatorTest.java
@@ -2,9 +2,21 @@ package no.ntnu.qos.server.mediators.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import no.ntnu.qos.server.mediators.MediatorConstants;
 import no.ntnu.qos.server.mediators.SAMLMediator;
 
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMXMLBuilderFactory;
+import org.apache.axiom.om.OMXMLParserWrapper;
+import org.apache.axiom.soap.SOAPBody;
+import org.apache.axis2.AxisFault;
 import org.apache.axis2.addressing.EndpointReference;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.config.SynapseConfiguration;
@@ -17,13 +29,35 @@ public class SAMLMediatorTest {
 	private static MessageContext synCtx;
 	private static final String CLIENT_ROLE = "client_role1";
 	private static final String SERVICE = "127.0.0.1";
-
+	private static final String SAML_BODY = "SAMLinSOAP.xml";
+	
 	@BeforeClass
-	public static void setupMessageContext(){
+	public static void setupMessageContext() throws FileNotFoundException, AxisFault{
 		synCtx = new Axis2MessageContext(new org.apache.axis2.context.MessageContext(), 
 				new SynapseConfiguration(), null);
-		//Configure the message context
+		//Configure the message context:
+		
+		//Add Endpoint
 		synCtx.setTo(new EndpointReference(SERVICE));
+		//Add SOAP envelope
+		synCtx.setEnvelope(OMAbstractFactory.getSOAP12Factory().createSOAPEnvelope());
+		//Add SOAP Header to envelope
+		synCtx.getEnvelope().addChild(OMAbstractFactory.getSOAP12Factory().createSOAPHeader());
+		//Create SOAP body
+		SOAPBody sb = OMAbstractFactory.getSOAP12Factory().createSOAPBody();
+		//Add SAML to SOAP body
+		createSAMLBody(sb);
+		//Add SOAP body to envelope
+		synCtx.getEnvelope().addChild(sb);
+		//System.out.println(synCtx.getEnvelope());
+	}
+	
+	private static void createSAMLBody(SOAPBody sb) throws FileNotFoundException{
+		InputStream in = new FileInputStream(new File(SAML_BODY));
+		OMXMLParserWrapper builder = OMXMLBuilderFactory.createOMBuilder(in);
+		OMElement ele = builder.getDocumentElement();
+		//System.out.println(ele);
+		sb.addChild(ele);
 	}
 
 	@Test
