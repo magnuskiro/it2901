39e832a8e3b7df7bf93273914af2d0459171ea81
Stig Tore Johannesen <stigtoj@stud.ntnu.no>
2012-03-13 22:15:49 +0100

    Initial JUnit test for token manager. Some skeleton methods needed to write test

---
+++ client/src/no/ntnu/qos/client/credentials/TokenImpl.java
@@ -0,0 +1,34 @@
+package no.ntnu.qos.client.credentials;
+
+import java.net.URI;
+
+public class TokenImpl implements Token{
+	//TODO: Change to accomodate an actual OpenSAML object?
+	String token;
+	long validUntil;
+	URI destination;
+	
+	public TokenImpl(String token, long validUntil, URI destination) {
+		this.token = token;
+		this.destination = destination;
+		this.validUntil = validUntil;
+	}
+	@Override
+	public String getXML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public URI getDestination() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
