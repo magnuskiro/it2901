7bce4b36a2f458535043be99293942122f935211
Haavard Toerresen <skabbkladden@gmail.com>
2012-03-15 13:29:22 +0100

    commented the isvalid-criteria in tokenImpl

--- client/src/no/ntnu/qos/client/credentials/impl/TokenImpl.java
+++ client/src/no/ntnu/qos/client/credentials/impl/TokenImpl.java
@@ -31,6 +31,9 @@ public class TokenImpl implements Token{
 
 	@Override
 	public boolean isValid() {
+		/* assumes that if the token became invalid less than 30 seconds ago, it is in practice OK to consider
+		 * it still valid, as requesting a new token might take longer than that, depending on the network
+		 * TODO: make it configurable*/
 		if(validUntil > (System.currentTimeMillis()-30000)) {
 			return true;
 		}
