778e61dcfbe177ae5bb404338b48f00948471c7e
Haavard Toerresen <skabbkladden@gmail.com>
2012-03-19 11:53:43 +0100

    DataObject will now send itself when it detects that the criteria for sending are met, as indicated by the send data sequence diagram

--- client/src/no/ntnu/qos/client/DataObject.java
+++ client/src/no/ntnu/qos/client/DataObject.java
@@ -8,6 +8,8 @@ import no.ntnu.qos.client.net.RouteInfo;
 
 /**
  * Object containing the data clients wish to send
+ * It will send itself when it detects that the criteria are met
+ * (is sane, has a token, has routing info)
  * @author Håvard
  *
  */
@@ -37,29 +39,43 @@ public class DataObject {
     }
 
     /**
-     * marks the data the object contains as sane
+     * marks the data the object contains as sane, sends itself if other
+     * criteria are met
      * @param sane	- true if data is sane
      */
     public void setSane(boolean sane){
         this.sane = sane;
+        
+        if (isReadyToSend()){
+        	sequencer.sendData(this);
+        }
     }
 
     /**
-     * sets the information on bandwidth and TR
+     * sets the information on bandwidth and TR, send itself if other
+     * criteria are met
      * @param routeInfo	- routingINfo object obtained from an msCommunicator
      */
     public void setRoutingInfo(RouteInfo routeInfo){
         this.routeInfo = routeInfo;
+        
+        if (isReadyToSend()){
+        	sequencer.sendData(this);
+        }
     }
 
     /**
-     * sets the clients SAML-token
+     * sets the clients SAML-token, sends itself if other criteria are met
      * @param token	- the Token
      */
     public void setToken(Token token){
         samlTok		= token;
         diffServ	= samlTok.getDiffServ();
         priority	= samlTok.getPriority();
+        
+        if (isReadyToSend()){
+        	sequencer.sendData(this);
+        }
     }
 
     /**
@@ -103,4 +119,14 @@ public class DataObject {
         return priority;
     }
 
+    /**
+     * checks if all necessary data/criteria for sending are present/met
+     * @return - true if ready to send, false if not 
+     */
+    private boolean isReadyToSend(){
+    	if(sane && routeInfo != null && samlTok != null){
+    		return true;
+    	}
+    	return false;
+    }
 }
