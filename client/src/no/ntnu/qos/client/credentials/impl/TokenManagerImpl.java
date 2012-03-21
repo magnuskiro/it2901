2933f8de86830dc4756d7d37554de57ff4666e98
Magnus Kirø <magnuskiro@gmail.com>
2012-03-21 12:55:18 +0100

    fixed setTokenInDataObject based on test failures. Done some more work on the TokenManagerTest

--- client/src/no/ntnu/qos/client/credentials/impl/TokenManagerImpl.java
+++ client/src/no/ntnu/qos/client/credentials/impl/TokenManagerImpl.java
@@ -2,15 +2,18 @@ package no.ntnu.qos.client.credentials.impl;
 
 import no.ntnu.qos.client.DataObject;
 import no.ntnu.qos.client.credentials.CredentialStorage;
+import no.ntnu.qos.client.credentials.SAMLCommunicator;
 import no.ntnu.qos.client.credentials.Token;
 import no.ntnu.qos.client.credentials.TokenManager;
+
+import java.util.NoSuchElementException;
 
 /**
  * @author Magnus Kirø - magnuskiro@ underdusken.no/gmail.com
  */
 public class TokenManagerImpl implements TokenManager {
 	private CredentialStorage credentialStorage;
-    SAMLCommunicatorImpl samlCommunicator;
+    SAMLCommunicator samlCommunicator;
 
     public TokenManagerImpl(String user, String role, String password) {
 		credentialStorage = new CredentialStorageImpl(user, role, password);
@@ -19,17 +22,28 @@ public class TokenManagerImpl implements TokenManager {
 
     @Override
 	public void setTokenInDataObject(DataObject dataObject) {
-        if(dataObject.getSamlToken()==null && credentialStorage.getToken(dataObject.getDestination())==null){
-            String[] credentials = credentialStorage.getCredentials();
-            Token token = samlCommunicator.getToken(dataObject.getDestination(), credentials[0], credentials[1], credentials[2]);
-            dataObject.setToken(token);
-            credentialStorage.storeToken(token);
+        if(dataObject.getSamlToken()!=null) return; // dataObject got Token, return.
+        try{
+            credentialStorage.getToken(dataObject.getDestination());
+            return;
+        }catch (NoSuchElementException e){
+            // now we know that the token does not exist so move on to creating it.
         }
+
+        String[] credentials = credentialStorage.getCredentials();
+        Token token = samlCommunicator.getToken(dataObject.getDestination(), credentials[0], credentials[1], credentials[2]);
+        dataObject.setToken(token);
+        credentialStorage.storeToken(token);
+
 	}
 
     @Override
     public void setCredentials(String username, String role, String password) {
         credentialStorage.setCredentials(username,role,password);
+    }
+    
+    public String[] getCredentials(){
+        return this.credentialStorage.getCredentials();
     }
 
     @Override
