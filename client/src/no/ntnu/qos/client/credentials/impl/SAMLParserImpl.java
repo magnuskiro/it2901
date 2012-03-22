64b8c50e5be627cb79ba615a9a838649a155cc49
Haavard Toerresen <skabbkladden@gmail.com>
2012-03-22 12:07:39 +0100

    created empty SAMLParserImpl

---
+++ client/src/no/ntnu/qos/client/credentials/impl/SAMLParserImpl.java
@@ -0,0 +1,14 @@
+package no.ntnu.qos.client.credentials.impl;
+
+import no.ntnu.qos.client.credentials.SAMLParser;
+import no.ntnu.qos.client.credentials.Token;
+
+public class SAMLParserImpl implements SAMLParser{
+
+	@Override
+	public Token tokenize(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
