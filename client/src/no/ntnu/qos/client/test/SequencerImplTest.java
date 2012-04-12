5c42f3c2bd019b8457438ec4349bda67f727a53a
Haavard Toerresen <skabbkladden@gmail.com>
2012-04-12 11:15:26 +0200

    one senddatata test in sequencerimpltest

--- client/src/no/ntnu/qos/client/test/SequencerImplTest.java
+++ client/src/no/ntnu/qos/client/test/SequencerImplTest.java
@@ -2,6 +2,7 @@ package no.ntnu.qos.client.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import no.ntnu.qos.client.ExceptionHandler;
 import no.ntnu.qos.client.ReceiveObject;
 import no.ntnu.qos.client.credentials.TokenManager;
@@ -50,8 +51,9 @@ public class SequencerImplTest {
         // returned
         ReceiveObject receiveObject;
 
-        //TODO - test this method.
-        assertFalse(true); // fails to remind us that the test is not complete. 
+        receiveObject = sequencer.sendData(data, destination);
+        
+        assertNotNull(receiveObject);
 
     }
 
