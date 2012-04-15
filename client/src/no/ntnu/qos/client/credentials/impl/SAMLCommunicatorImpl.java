f54a74189a1eba10c8ee38e2d72c23b636a382ed
Ola Martin Tiseth Støvneng <ola.martin.st@gmail.com>
2012-04-15 15:43:54 +0200

    ROLE!!

--- client/src/no/ntnu/qos/client/credentials/impl/SAMLCommunicatorImpl.java
+++ client/src/no/ntnu/qos/client/credentials/impl/SAMLCommunicatorImpl.java
@@ -62,7 +62,17 @@ public class SAMLCommunicatorImpl implements SAMLCommunicator {
 						"NameFormat=\"urn:oasis:names:tc:SAML:2.0:attrname-format:uri\" " +
 						"Name=\"urn:oid:1.3.6.1.4.1.5923.1.1.1.1\" " +
 						"FriendlyName=\"qosClientRole\">" +
-						"<saml2:AttributeValue>clientRole1</saml2:AttributeValue>" +
+						"<saml2:AttributeValue>"+
+						/*
+						 * 
+						 * 
+						 * 
+						 *DO NOT FUCKING REMOVE ROLE!!!
+						 *
+						 *
+						 *
+						 */
+						role+"</saml2:AttributeValue>" +
 					"</saml2:Attribute>" +
 				"</saml2:AttributeStatement>" +
 			"</saml2:Assertion>";
